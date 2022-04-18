4fd9bfab21c8e2b1e844d9503871c53c9bbe8f13
devanomaly <rafaelfpcosta@gmail.com>
2022-04-18 18:17:44 -0300

    Deploy React app to GHPages... again

--- src/components/Scroll.jsx
+++
@@ -1,7 +0,0 @@
-export default function Scroll(props) {
-  return (
-    <div style={{overflowY: 'scroll', border: '1px solid black', height: '600px'}}>
-      {props.children}
-    </div>
-  )
-}
--- src/services/string-in-object-vals.js
+++
@@ -1,7 +0,0 @@
-// Given a string and an object, searches for any occurrences of the former in the latter's values. 
-// returns true or false
-
-export const searchStringInObjectValues = (someString, someObject) => 
-someString === '' ? true : Object.values(someObject).some(function (val) {
-  return val.toString().toLowerCase().includes(someString.toLowerCase())
-})
